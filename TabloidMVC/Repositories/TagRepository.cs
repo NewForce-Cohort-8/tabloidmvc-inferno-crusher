3a5648607db21adf98d2b969f25b2d7480b7e0a4
A-Nya Badger <asbadger01@gmail.com>
2024-01-25 14:13:17 -0500

    Added create tag functionality

--- TabloidMVC/Repositories/TagRepository.cs
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -100,38 +100,27 @@ namespace TabloidMVC.Repositories
         }
     }
 }
+public void Add(Tag tag)
+{
+    using (var conn = Connection)
+    {
+        conn.Open();
+        using (var cmd = conn.CreateCommand())
+        {
+            cmd.CommandText =@"
+                        INSERT INTO Tag ([Name])
+                    OUTPUT INSERTED.ID
+                    VALUES (@name);";
+            cmd.Parameters.AddWithValue("@name", tag.Name);
+            
+            tag.Id = (int)cmd.ExecuteScalar();
+                  }
+    }
+}
     }
 }
 
 
-//        public void Add(Post post)
-//        {
-//            using (var conn = Connection)
-//            {
-//                conn.Open();
-//                using (var cmd = conn.CreateCommand())
-//                {
-//                    cmd.CommandText = @"
-//                        INSERT INTO Post (
-//                            Title, Content, ImageLocation, CreateDateTime, PublishDateTime,
-//                            IsApproved, CategoryId, UserProfileId )
-//                        OUTPUT INSERTED.ID
-//                        VALUES (
-//                            @Title, @Content, @ImageLocation, @CreateDateTime, @PublishDateTime,
-//                            @IsApproved, @CategoryId, @UserProfileId )";
-//                    cmd.Parameters.AddWithValue("@Title", post.Title);
-//                    cmd.Parameters.AddWithValue("@Content", post.Content);
-//                    cmd.Parameters.AddWithValue("@ImageLocation", DbUtils.ValueOrDBNull(post.ImageLocation));
-//                    cmd.Parameters.AddWithValue("@CreateDateTime", post.CreateDateTime);
-//                    cmd.Parameters.AddWithValue("@PublishDateTime", DbUtils.ValueOrDBNull(post.PublishDateTime));
-//                    cmd.Parameters.AddWithValue("@IsApproved", post.IsApproved);
-//                    cmd.Parameters.AddWithValue("@CategoryId", post.CategoryId);
-//                    cmd.Parameters.AddWithValue("@UserProfileId", post.UserProfileId);
-
-//                    post.Id = (int)cmd.ExecuteScalar();
-//                }
-//            }
-//        }
 
 //        private Post NewPostFromReader(SqlDataReader reader)
 //        {
